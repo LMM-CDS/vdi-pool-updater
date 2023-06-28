dffd04e608097320337619ae18138373e17b474c
LAPLAUD Francois <elfrancesco+git@gmail.com>
2023-06-28 15:45:14 +0200

    add sample config

---
+++ config-sample.ps1
@@ -0,0 +1,9 @@
+$ProjectRoot = $PSScriptRoot
+$LogPath = "$ProjectRoot\logs"
+$LibPath = "$ProjectRoot\libs"
+$TestDataPath = "$ProjectRoot\data\testdata"
+
+$VDI_SERVER = "<VDI SERVER FQDN>"
+$VCENTER_SERVER = "<VCENTER SERVER FQDN>"
+$SMS_SERVER = "<SMS SERVER>"
+$SMS_SiteCode = "<SITE CODE>"
